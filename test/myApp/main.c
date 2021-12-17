3df7575832a8dca11a1fc4920f43b3b27d06300f
Hyungkyu Lee <leehyungkyutom@gmail.com>
2021-12-17 15:22:18 -0500

    testing

---
+++ test/myApp/main.c
@@ -0,0 +1,8 @@
+/* vis.c */
+#include "main.h"
+
+
+int main(void) {
+  numYear(2);
+  return 0;
+}
---
+++ test/myApp/visual.c
@@ -0,0 +1,33 @@
+/* visual.c */
+#include "visual.h"
+
+
+int printSS(){
+  double row, col;
+  getmaxyx(stdscr,row,col);
+  row = row / 2;
+  col = col / 2;
+  move(row,col);
+  printw("s");
+
+  for(int i = 0; i < PlanetNum; i++){
+    printPlanet(getcA(i), row, col);
+  }
+  sleep(1);
+  return SUCESS;
+}
+
+int printPlanet(double cA, double r, double c){
+  move(r - 2 * sin(cA), c - 2 * cos(cA));
+  return SUCESS;
+}
+
+int numYear(int year){
+  initscr();
+  init();
+  for(int i = 0; i < year; i++){
+    printSS();
+  }
+  endwin();
+  return SUCESS;
+}
